f436f3f3c80b32fd91f36a6a40ca6505c1a476ef
haesooKr <gotn676@gmail.com>
2019-12-26 17:54:20 -0500

    Function to save name on Local Storage

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(e){
+    e.preventDefault();
+    const currentInput = input.value;
+    colorGreeting(currentInput);
+    saveName(currentInput);
+}
+
+function askName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
+}
+
 function colorGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ function colorGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS)
     if(currentUser === null){
-
+        askName();
     } else{
         colorGreeting(currentUser);
     }
