2ae2b5f776d76edd4e102b7fc053e1c6b77117b1
haesooKr <gotn676@gmail.com>
2019-12-26 20:56:00 -0500

    Show Temperature and Place

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+console.log(weather);
+
 const COORDS = 'coords';
+const API_KEY = "ea6f69a96da7159d3a1bffea7f43846a"
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`)
+    .then(function(response){
+        return (response.json());
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}° in ${place}`
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSuccess(position){
         longitude
     }
     saveCoords(coordsObj);
+    getWehater(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -27,7 +43,8 @@ function loadCoords(){
     if(loadedCords === null){
         askForCoords();
     } else{
-
+        const parseCoords = JSON.parse(loadedCords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
